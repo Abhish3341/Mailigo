480b92d9c108f0b41ae66ad8e81df8c06865f4ec
Abhish3341 <abhinavaddress@gmail.com>
2025-04-19 03:12:19 +0530

    changes some crededentials

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="228133815033-r218oa3qqc7h9muonbvu3tel36cv98jt.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="630867434044-pf58bmcve333sesdunhr373ttu7mm5l8.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
