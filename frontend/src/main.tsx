93230dbace625445a996aa9c45741fe5ff1b85ef
Abhish3341 <abhinavaddress@gmail.com>
2025-04-19 04:21:18 +0530

    solved issue with credentials and its name

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="630867434044-pf58bmcve333sesdunhr373ttu7mm5l8.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="48387091157-kim367qmqgdtg86lnp8a28b2u8f8bl1o.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
