c02f11469178d034439d4c8c52d1c14d39ddfe4c
Abhish3341 <abhinavaddress@gmail.com>
2025-04-19 06:23:23 +0530

    some changes to the database connection and package-lock.json in backend

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,16 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // Set strictQuery to false to prepare for Mongoose 7
+        // Optional: Set strictQuery to false (still okay for Mongoose 7)
         mongoose.set('strictQuery', false);
 
-        const conn = await mongoose.connect(process.env.MONGODB_URI , {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
             serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
             family: 4 // Use IPv4, skip trying IPv6
         });
-        
+
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error('MongoDB connection error:', error);
